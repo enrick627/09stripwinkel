90b9907cf45ae8a77467ae38adf9180348ed8087
Enrick De Munter <enrick.demunter@campus.romerocollege.be>
2019-10-18 10:55:42 +0200

    Add project files.

---
+++ stripwinkel/Program.cs
@@ -0,0 +1,32 @@
+﻿// Enrick De Munter 5ITN nr4 18/10/2019
+/*Opgave
+ * Tijdens een uitverkoop bij een stripwinkel bedraagt de prijs van elke strip 5 euro exclusief btw (hint: zoek dit tarief op voor België).
+ * In de winkel staan verschillende computers waarop de gebruiker hun aantal stripboeken kunnen ingeven.
+ * Het programma laat de berekeningen zien en het totaal te betalen bedrag.
+ * De gebruiker moet ook zijn voornaam ingeven. Ontwerp een consoletoepassing voor de klanten.
+ * De weergave moet er als volgt uitzien: Welkom … ! 
+ *
+ * Analyse
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stripwinkel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
